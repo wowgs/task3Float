acee446c18586ddce9d376616b3617044d5f05f1
nfadeeva <fadeevanastasiya@gmail.com>
2015-10-22 22:53:05 +0300

    float1

---
+++ float1.c
@@ -0,0 +1,46 @@
+union floatInt{
+    unsigned int i;
+    float f;
+};
+
+int main()
+{
+    int i = 0;
+    float number;
+
+    union floatInt u;
+    scanf("%f",&number);
+    u.f = number;
+
+    int sign = u.i >> 31 &1;
+    int exp = u.i >> 23 & 0xff;
+    int m = u.i & (0x7fffff);
+
+    if (exp == 0xff)
+    {
+        if (m = 0)
+        {
+            printf((sign ? "-inf" : "+inf"));
+        }
+
+        else
+        {
+            printf("NaN");
+        }
+    }
+    else
+    {
+        printf("%5d %28c%-d\n",sign,' ',exp-127);
+        printf("(-1) * 1.");
+        for (i = 22; i > 0; i--)
+        {
+            printf("%d", (m >> i)& 1);
+        }
+        printf(" * 2");
+
+    }
+
+
+    system("pause");
+    return 0;
+}
