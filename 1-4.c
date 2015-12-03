d4a0237467aba4001598fc15726a240e041f5afb
nfadeeva <fadeevanastasiya@gmail.com>
2015-12-04 02:43:09 +0300

    Update 1-4.c

--- 1-4.c
+++ 1-4.c
@@ -43,7 +43,7 @@ size_t strcmp(char *s1, char *s2)
   {
       int i = 0;
       int k = 0;
-      while (s1[i] == s2[i])
+      while ((s1[i] == s2[i]) && (s1[i] != '\0') && (s2[i] != '\0'))
              {
                    i++;
              };
